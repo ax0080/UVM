bad114dd538626c6ea5e95e071645efc31c3e6f5
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-11-23 14:25:50 +0800

    Add files via upload

---
+++ Code/ch2/section2.3/2.3.4/run_rtl.f
@@ -0,0 +1,17 @@
++access+rw
+-sv
+-uvm
++incdir+./
+-disable_sem2009
+-timescale 1ns/10ps
+
+./dut.sv
+./my_if.sv
+./my_transaction.sv
+./my_driver.sv
+./my_monitor.sv
+./my_agent.sv
+./my_env.sv
+./top_tb.sv
+
+
