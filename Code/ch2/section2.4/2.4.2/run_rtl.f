dff9be5a1bd5e078e43ee63260cc9dac395cba89
謝旻言 <78300584+ax0080@users.noreply.github.com>
2023-12-05 09:58:55 +0800

    Add files via upload

---
+++ Code/ch2/section2.4/2.4.2/run_rtl.f
@@ -0,0 +1,21 @@
++access+rw
+-sv
+-uvm
++incdir+./
+-disable_sem2009
+-timescale 1ns/10ps
+
+./dut.sv
+./my_if.sv
+./my_transaction.sv
+./my_driver.sv
+./my_monitor.sv
+./my_model.sv
+./my_sequence.sv
+./my_sequencer.sv
+./my_scoreboard.sv
+./my_agent.sv
+./my_env.sv
+./top_tb.sv
+
+
